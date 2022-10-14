718484c96531b40def6233f593154b036a3d1b93
Nikita Sapogov <amstaffix@gmail.com>
2022-10-14 22:06:43 +0300

    changed config options to proper ones

--- clientside/plugins/rotations/paladin/protection/v2/01_amstaffix_paladin_protection_loader.lua
+++ clientside/plugins/rotations/paladin/protection/v2/01_amstaffix_paladin_protection_loader.lua
@@ -22,24 +22,24 @@ local ROTATION_LINK = "TODO"
 local Config = {
     ---Toggle debug mode. Turn on, if you encounter some issues and want to deal with it, or record a video and send
     ---to author.
-    debug = true,
+    debug = false,
     ---Use standard CombatRotation pluggable function. Change only if you know what you are doing.
     useCombatRotationLauncher = true,
     ---Use online loading feature to get last updates
-    onlineLoad = false,
+    onlineLoad = true,
 
     useConsecrations = true,
     useConsecrationsMinEnemies = 1,
 
     groupCleanseModEnabled = false,
     useJudgmentType = 1, -- 1:Judgement of Light; 2:Judgement of Wisdom; 3:Judgement of Justice;
-    useJudgmentTryToCleave = true,
+    useJudgmentTryToCleave = false,
     useJudgmentForDebuffOnly = false,
 
     useHandOfReckoningToMakeDamage = true,
     useHandOfReckoningInInstance = true,
 
-    defaultAuraToUse = 3, -- 1:Devotion Aura; 2:Retribution Aura; 3:Concentration Aura; 4:Shadow Resistance Aura; 5:Frost Resistance Aura; 6:Fire Resistance Aura
+    defaultAuraToUse = 1, -- 1:Devotion Aura; 2:Retribution Aura; 3:Concentration Aura; 4:Shadow Resistance Aura; 5:Frost Resistance Aura; 6:Fire Resistance Aura
     defaultAuraChangeIfAlreadyExist = { 2, 3 }, -- 1:Devotion Aura; 2:Retribution Aura; 3:Concentration Aura; 4:Shadow Resistance Aura; 5:Frost Resistance Aura; 6:Fire Resistance Aura
     defaultBlessingToUse = 4, -- 1:Blessing of Might; 2:Blessing of Kings; 3:Blessing of Wisdom; 4:Blessing of Sanctuary;
     defaultSealToUse = 4, -- 1:Seal of Righteousness; 2:Seal of Justice; 3:Seal of Light; 4:Seal of Wisdom; 5:Seal of Command; 6:Seal of Corruption;
